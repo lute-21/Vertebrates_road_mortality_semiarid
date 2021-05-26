c8a5e4c74f36962f0f8a6c9c82ed7895226f0467
lutebio@outlook.com <lutebio2009@gmail.com>
2021-05-26 15:32:02 -0300

    intial commit

--- R/01_C_load_data.R
+++ R/01_C_load_data.R
@@ -5,10 +5,10 @@ library(here)
 
 #reading data-----------------------------------------------------------------
 
-raw_data<-read.csv(here("data","raw","Data_October_2017.csv"),sep= ";")
+raw_data<-read.csv(here("data","raw","Data_October_2017.csv"),sep= ";", header= T)
 
 #read results-----------------------------------------------------------------
-
+raw_data
 
 
 
